a54ca909174a33322b609ca2a2ced445504c6c4f
Gituser143 <bhargavsnv100@gmail.com>
2020-07-24 03:18:51 +0530

    Add functionality to display Memory and CPU usage values

--- src/main.go
+++ src/main.go
@@ -3,43 +3,95 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"strconv"
+	"sync"
+	"time"
 
-	"github.com/shirou/gopsutil/process"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 )
+
+var wg sync.WaitGroup
+
+func printCPURates(cpuRates []float64) {
+	var outputString string
+	for index, rate := range cpuRates {
+		outputString = outputString + "CPU" + strconv.Itoa(index) + ": " + strconv.Itoa(int(rate)) + "% "
+	}
+	fmt.Println(outputString)
+}
+
+func printMemRates(memory *mem.VirtualMemoryStat) {
+	fmt.Println("Total:", float32(memory.Total)/(1024*1024*1024), "Avaiabe:", float32(memory.Available)/(1024*1024*1024), "Used:", float32(memory.Used)/(1024*1024*1024))
+}
+
+// Function to print out CPU usages and memory values
+func globalStats(endChannel chan int) {
+	for {
+		select {
+		case <-endChannel: // Stop execution if end signal received
+
+			wg.Done()
+			return
+
+		default: // Get Memory and CPU rates per core for every 1 second
+
+			cpuUsageRates, err := cpu.Percent(1*time.Second, true)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			memoryStat, err := mem.VirtualMemory()
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			// Do something with values, just printing for now
+			printCPURates(cpuUsageRates)
+			printMemRates(memoryStat)
+			println()
+		}
+	}
+}
 
 func main() {
 
-	if len(os.Args) != 2 {
-		fmt.Println("PID not entered!")
-		os.Exit(1)
-	}
+	endChannel := make(chan int, 1) // Channel to signal end of routine
 
-	arg, _ := strconv.Atoi(os.Args[1])
-	pid := int32(arg)
+	wg.Add(1)                  // Increment semaphore by 1 to allow new routine
+	go globalStats(endChannel) // Launch routine
 
-	myProcess, err := process.NewProcess(pid)
-	if err != nil {
-		log.Fatal(err)
-	}
+	time.Sleep(10 * time.Second) // A galeej way to keep the main routine busy
+	endChannel <- 1              // Send signal for routine to stop
 
-	for {
-		status, _ := myProcess.IsRunning()
+	// if len(os.Args) != 2 {
+	// 	fmt.Println("PID not entered!")
+	// 	os.Exit(1)
+	// }
+	// arg, _ := strconv.Atoi(os.Args[1])
+	// pid := int32(arg)
 
-		if status == true {
-
-			cpu_percent, err := myProcess.CPUPercent()
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			mem_percent, err := myProcess.MemoryPercent()
-			if err != nil {
-				log.Fatal(err)
-			}
-			fmt.Println("CPU Percent: ", cpu_percent, " Memory Percent: ", mem_percent)
-		}
-
-	}
+	// myProcess, err := process.NewProcess(pid)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	//
+	// for {
+	// 	status, _ := myProcess.IsRunning()
+	//
+	// 	if status == true {
+	//
+	// 		cpu_percent, err := myProcess.CPUPercent()
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+	//
+	// 		mem_percent, err := myProcess.MemoryPercent()
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+	// 		fmt.Println("CPU Percent: ", cpu_percent, " Memory Percent: ", mem_percent)
+	// 	}
+	//
+	// }
 }
