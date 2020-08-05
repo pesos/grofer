4fea49d1be211391226c5970d3bc9905960f8fc4
MadhavJivrajani <madhav.jiv@gmail.com>
2020-08-06 00:13:12 +0530

    added license to new files and fixed date test

--- src/utils/utils_test.go
+++ src/utils/utils_test.go
@@ -1,7 +1,24 @@
+/*
+Copyright © 2020 The PES Open Source Team pesos@pes.edu
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package utils_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/pesos/grofer/src/utils"
 )
@@ -60,14 +77,26 @@ func TestGetInMB(t *testing.T) {
 }
 
 func TestGetDateFromUnix(t *testing.T) {
+	t1 := time.Unix(10000000, 0)
+	date1 := t1.Format(time.RFC822)
+
+	t2 := time.Unix(0, 0)
+	date2 := t2.Format(time.RFC822)
+
+	t3 := time.Unix(1596652055, 0)
+	date3 := t3.Format(time.RFC822)
+
+	t4 := time.Unix(9999999999, 0)
+	date4 := t4.Format(time.RFC822)
+
 	tests := []struct {
 		inputVal    int64
 		expectedVal string
 	}{
-		{10000000, "26 Apr 70 23:16 IST"},
-		{0, "01 Jan 70 05:30 IST"},
-		{1596652055, "05 Aug 20 23:57 IST"},
-		{9999999999, "20 Nov 86 23:16 IST"},
+		{10000000, date1},
+		{0, date2},
+		{1596652055, date3},
+		{9999999999, date4},
 	}
 
 	for _, test := range tests {
