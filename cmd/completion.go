82f4568370f4732abf440ced911f3dbe9729f9ab
Anirudh H M <anihm136@gmail.com>
2020-09-30 21:19:37 +0530

    Clean up description and remove generic stuff

--- cmd/completion.go
+++ cmd/completion.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2020 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -23,19 +8,19 @@ import (
 
 // completionCmd represents the completion command
 var completionCmd = &cobra.Command{
-	Use:                   "completion [bash|zsh|fish|powershell]",
-	Short:                 "Generate completion script",
+	Use:   "completion [bash|zsh|fish|powershell]",
+	Short: "Generate completion script",
 	Long: `To load completions:
 
 Bash:
 
-$ source <(yourprogram completion bash)
+$ source <(grofer completion bash)
 
 # To load completions for each session, execute once:
 Linux:
-	$ yourprogram completion bash > /etc/bash_completion.d/yourprogram
+	$ grofer completion bash > /etc/bash_completion.d/grofer 
 MacOS:
-	$ yourprogram completion bash > /usr/local/etc/bash_completion.d/yourprogram
+	$ grofer completion bash > /usr/local/etc/bash_completion.d/grofer 
 
 Zsh:
 
@@ -45,45 +30,34 @@ Zsh:
 $ echo "autoload -U compinit; compinit" >> ~/.zshrc
 
 # To load completions for each session, execute once:
-$ yourprogram completion zsh > "${fpath[1]}/_yourprogram"
+$ yourprogram completion zsh > "${fpath[1]}/_grofer"
 
 # You will need to start a new shell for this setup to take effect.
 
 Fish:
 
-$ yourprogram completion fish | source
+$ grofer completion fish | source
 
 # To load completions for each session, execute once:
-$ yourprogram completion fish > ~/.config/fish/completions/yourprogram.fish
+$ grofer completion fish > ~/.config/fish/completions/grofer.fish
 `,
-DisableFlagsInUseLine: true,
-ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
-Args:                  cobra.ExactValidArgs(1),
-Run: func(cmd *cobra.Command, args []string) {
-	switch args[0] {
-	case "bash":
-		cmd.Root().GenBashCompletion(os.Stdout)
-	case "zsh":
-		cmd.Root().GenZshCompletion(os.Stdout)
-	case "fish":
-		cmd.Root().GenFishCompletion(os.Stdout, true)
-	case "powershell":
-		cmd.Root().GenPowerShellCompletion(os.Stdout)
-	}
-},
+	DisableFlagsInUseLine: true,
+	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+	Args:                  cobra.ExactValidArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		switch args[0] {
+		case "bash":
+			cmd.Root().GenBashCompletion(os.Stdout)
+		case "zsh":
+			cmd.Root().GenZshCompletion(os.Stdout)
+		case "fish":
+			cmd.Root().GenFishCompletion(os.Stdout, true)
+		case "powershell":
+			cmd.Root().GenPowerShellCompletion(os.Stdout)
+		}
+	},
 }
 
 func init() {
 	rootCmd.AddCommand(completionCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// completionCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// completionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
