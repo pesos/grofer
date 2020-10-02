d5b0399d951cb2c205a5516a4e5e06998302084b
Gituser143 <bhargavsnv100@gmail.com>
2020-10-02 23:17:55 +0530

    Added cute ascii art!

--- src/utils/doggo.go
+++ src/utils/doggo.go
@@ -18,9 +18,28 @@ package utils
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func ErrorMsg() {
+	rand.Seed(time.Now().UnixNano())
+	num := rand.Intn(5)
+	switch num {
+	case 0:
+		ErrorDoggo()
+	case 1:
+		ErrorCatto()
+	case 2:
+		ErrorBunny()
+	case 3:
+		ErrorDolphy()
+	case 4:
+		ErrorOwl()
+	}
+}
+
+func ErrorDoggo() {
 	dog := `
 	pid no exist, done doggo a sad
 		\
@@ -35,4 +54,88 @@ func ErrorMsg() {
 		  || (___\====
 	`
 	fmt.Println(dog)
+}
+
+func ErrorOwl() {
+	goobes := `
+
+The council of wise owls are confused! Please provide a valid PID!
+
+   /\_/\       /\_/\        /\_/\        /\_/\
+  ((@v@))     ((@v@))      ((@v@))      ((@v@))
+ ():::::()   ():::::()    ():::::()    ():::::()
+   VV-VV       VV-VV        VV-VV        VV-VV
+  `
+	fmt.Println(goobes)
+}
+
+func ErrorCatto() {
+	cat := `
+Catto says PID is invalid, plis give valid PID
+    \
+     \
+  　██░▀██████████████▀░██
+　　█▌▒▒░████████████░▒▒▐█
+　　█░▒▒▒░██████████░▒▒▒░█
+　　▌░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░▐
+　　░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░
+　 ███▀▀▀██▄▒▒▒▒▒▒▒▄██▀▀▀██
+　 ██░░░▐█░▀█▒▒▒▒▒█▀░█▌░░░█
+　 ▐▌░░░▐▄▌░▐▌▒▒▒▐▌░▐▄▌░░▐▌
+　　█░░░▐█▌░░▌▒▒▒▐░░▐█▌░░█
+　　▒▀▄▄▄█▄▄▄▌░▄░▐▄▄▄█▄▄▀▒
+　　░░░░░░░░░░└┴┘░░░░░░░░░
+　　██▄▄░░░░░░░░░░░░░░▄▄██
+　　████████▒▒▒▒▒▒████████
+　　█▀░░███▒▒░░▒░░▒▀██████
+　　█▒░███▒▒╖░░╥░░╓▒▐█████
+　　█▒░▀▀▀░░║░░║░░║░░█████
+　　██▄▄▄▄▀▀┴┴╚╧╧╝╧╧╝┴┴███
+　　██████████████████████`
+	fmt.Println(cat)
+}
+
+func ErrorDolphy() {
+	dolphy := `
+                               _.-~  )
+                    _..--~~~~,'   ,-/     _
+                 .-'. . . .'   ,-','    ,' )
+               ,'. . . _   ,--~,-'__..-'  ,'
+             ,'. . .  (@)' ---~~~~      ,'
+            /. . . . '~~             ,-'
+           /. . . . .             ,-'
+          ; . . . .  - .        ,'
+         : . . . .      \_     /       PID did Dolphy a daze,
+        . . . . .          \-.:        Please enter valid PID
+       . . . ./  - .          )
+      .  . . |  _____..---.._/ _____________
+~---~~~~----~~~~             ~~
+`
+	fmt.Println(dolphy)
+}
+
+func ErrorBunny() {
+	bunny := `             ,
+            /|      __
+           / |   ,-~ /
+          Y :|  //  /
+          | jj /( .^
+          >-"~"-v"
+         /       Y
+        jo  o    |
+       ( ~T~     j
+        >._-' _./
+       /   "~"  |
+      Y     _,  |
+     /| ;-"~ _  l
+    / l/ ,-"~    \
+    \//\/      .- \
+     Y        /    Y     Bunny couldn't recognise that PID.
+     l       I     !      Done bunny a confuse.
+     ]\      _\    /"\     Please give bunny a valid PID.
+    (" ~----( ~   Y.  )
+~~~~~~~~~~~~~~~~~~~~~~~~~
+  `
+
+	fmt.Println(bunny)
 }
