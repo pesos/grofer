fed51c81a160fed5e5f93979e7727ec606b08195
Gituser143 <bhargavsnv100@gmail.com>
2021-01-04 12:53:49 +0530

    Add binary compile script

---
+++ compile.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+archs=("amd64" "386" "arm" "arm64")
+
+mkdir -p bin
+
+for arch in ${archs[@]}; do
+  env GOOS=linux GOARCH=${arch} go build ./grofer.go
+  cp grofer bin/grofer_${arch}
+  echo "Compiled grofer_${arch}"
+done
