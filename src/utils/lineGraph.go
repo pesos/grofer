bc91de6c74d227de0a60c504557f66bf6c035343
Bhargav SNV <44526455+Gituser143@users.noreply.github.com>
2021-05-06 23:35:57 +0530

    Update src/utils/lineGraph.go
    Co-authored-by: Madhav Jivrajani <madhav.jiv@gmail.com>

--- src/utils/lineGraph.go
+++ src/utils/lineGraph.go
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
+// This particularar widget is inspired and borrowed from the implementation of https://github.com/cjbassi/gotop
 package utils
 
 import (
