e91b0c584c80c4dadaed2a6e72d5c0095a7feb2c
Gituser143 <bhargavsnv100@gmail.com>
2021-08-22 21:08:40 +0530

    Change SecondsToHuman string format

--- pkg/utils/data_format.go
+++ pkg/utils/data_format.go
@@ -30,6 +30,7 @@ var (
 	Q = math.Pow(10, 15)
 )
 
+// Convert a given UNIX epoch in seconds to human readble format
 func SecondsToHuman(input int) (result string) {
 	years := math.Floor(float64(input) / 60 / 60 / 24 / 7 / 30 / 12)
 	seconds := input % (60 * 60 * 24 * 7 * 30 * 12)
@@ -45,7 +46,7 @@ func SecondsToHuman(input int) (result string) {
 	seconds = input % 60
 
 	if years > 0 {
-		result = fmt.Sprintf("%d y %d m %d w %d d %d H %d M %d S",
+		result = fmt.Sprintf("%dy %dm %dw %dd %dH %dM %dS",
 			int(years),
 			int(months),
 			int(weeks),
@@ -55,7 +56,7 @@ func SecondsToHuman(input int) (result string) {
 			int(seconds),
 		)
 	} else if months > 0 {
-		result = fmt.Sprintf("%d m %d w %d d %d H %d M %d S",
+		result = fmt.Sprintf("%dm %dw %dd %dH %dM %dS",
 			int(months),
 			int(weeks),
 			int(days),
@@ -64,7 +65,7 @@ func SecondsToHuman(input int) (result string) {
 			int(seconds),
 		)
 	} else if weeks > 0 {
-		result = fmt.Sprintf("%d w %d d %d H %d M %d S",
+		result = fmt.Sprintf("%dw %dd %dH %dM %dS",
 			int(weeks),
 			int(days),
 			int(hours),
@@ -72,25 +73,25 @@ func SecondsToHuman(input int) (result string) {
 			int(seconds),
 		)
 	} else if days > 0 {
-		result = fmt.Sprintf("%d d %d H %d M %d S",
+		result = fmt.Sprintf("%dd %dH %dM %dS",
 			int(days),
 			int(hours),
 			int(minutes),
 			int(seconds),
 		)
 	} else if hours > 0 {
-		result = fmt.Sprintf("%d H %d M %d S",
+		result = fmt.Sprintf("%dH %dM %dS",
 			int(hours),
 			int(minutes),
 			int(seconds),
 		)
 	} else if minutes > 0 {
-		result = fmt.Sprintf("%d M %d S",
+		result = fmt.Sprintf("%dM %dS",
 			int(minutes),
 			int(seconds),
 		)
 	} else {
-		result = fmt.Sprintf("%d S",
+		result = fmt.Sprintf("%dS",
 			int(seconds),
 		)
 	}
@@ -103,6 +104,7 @@ func roundOffNearestTen(num float64, divisor float64) float64 {
 	return math.Round(x*10) / 10
 }
 
+// Round off values to nearest K, G, M, etc. Returns the rounded values and the unit. If inBytes is set to true, units are returned as B, kB, gB, etc.
 func RoundValues(num1, num2 float64, inBytes bool) ([]float64, string) {
 	nums := []float64{}
 	var units string
@@ -182,13 +184,14 @@ func GetInMB(bytes uint64, precision int) float64 {
 	return Trim(temp, precision)
 }
 
-// GetDateFromUnix gets a date and time in RFC822 format from a unix epoch
+// GetDateFromUnix gets a date and time in RFC822 format from a unix epoch in millisecond
 func GetDateFromUnix(createTime int64) string {
 	t := time.Unix(createTime/1000, 0)
 	date := t.Format(time.RFC822)
 	return date
 }
 
+// RoundFloat rounds off a float to a given base and precision
 func RoundFloat(num float64, base string, precision int) float64 {
 	x := num
 	div := math.Pow10(precision)
@@ -203,6 +206,7 @@ func RoundFloat(num float64, base string, precision int) float64 {
 	return math.Round(x*div) / div
 }
 
+// RoundUint rounds a float by making use of RoundFloat
 func RoundUint(num uint64, base string, precision int) float64 {
 	x := float64(num)
 	return RoundFloat(x, base, precision)
