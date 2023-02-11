3bd5700f251e0d69300e61318132c629e2aeb693
BhautikChudasama <bhautikrchudasama@gmail.com>
2023-02-11 10:41:13 +0530

    refactor: refactored signal code

--- pkg/sink/tui/misc/signals.go
+++ pkg/sink/tui/misc/signals.go
@@ -1,5 +1,3 @@
-//go:build android || aix || dragonfly || freebsd || (js && wasm) || linux || nacl || netbsd || plan9 || windows
-
 /*
 Copyright © 2020 The PES Open Source Team pesos@pes.edu
 
@@ -166,44 +164,6 @@ var allSignals = [][]string{
 		"35",
 		"SIGXFSZ",
 	},
-}
-
-var signalMap = map[string]syscall.Signal{
-	"SIGABRT":   syscall.SIGABRT,
-	"SIGALRM":   syscall.SIGALRM,
-	"SIGBUS":    syscall.SIGBUS,
-	"SIGCHLD":   syscall.SIGCHLD,
-	"SIGCLD":    syscall.SIGCLD,
-	"SIGCONT":   syscall.SIGCONT,
-	"SIGFPE":    syscall.SIGFPE,
-	"SIGHUP":    syscall.SIGHUP,
-	"SIGILL":    syscall.SIGILL,
-	"SIGINT":    syscall.SIGINT,
-	"SIGIO":     syscall.SIGIO,
-	"SIGIOT":    syscall.SIGIOT,
-	"SIGKILL":   syscall.SIGKILL,
-	"SIGPIPE":   syscall.SIGPIPE,
-	"SIGPOLL":   syscall.SIGPOLL,
-	"SIGPROF":   syscall.SIGPROF,
-	"SIGPWR":    syscall.SIGPWR,
-	"SIGQUIT":   syscall.SIGQUIT,
-	"SIGSEGV":   syscall.SIGSEGV,
-	"SIGSTKFLT": syscall.SIGSTKFLT,
-	"SIGSTOP":   syscall.SIGSTOP,
-	"SIGSYS":    syscall.SIGSYS,
-	"SIGTERM":   syscall.SIGTERM,
-	"SIGTRAP":   syscall.SIGTRAP,
-	"SIGTSTP":   syscall.SIGTSTP,
-	"SIGTTIN":   syscall.SIGTTIN,
-	"SIGTTOU":   syscall.SIGTTOU,
-	"SIGUNUSED": syscall.SIGUNUSED,
-	"SIGURG":    syscall.SIGURG,
-	"SIGUSR1":   syscall.SIGUSR1,
-	"SIGUSR2":   syscall.SIGUSR2,
-	"SIGVTALRM": syscall.SIGVTALRM,
-	"SIGWINCH":  syscall.SIGWINCH,
-	"SIGXCPU":   syscall.SIGXCPU,
-	"SIGXFSZ":   syscall.SIGXFSZ,
 }
 
 const sigNameIdx int = 1
---
+++ pkg/sink/tui/misc/signals_list_others.go
@@ -0,0 +1,43 @@
+//go:build android || aix || dragonfly || freebsd || (js && wasm) || linux || nacl || netbsd || plan9 || windows
+
+package misc
+
+import "syscall"
+
+var signalMap = map[string]syscall.Signal{
+	"SIGABRT":   syscall.SIGABRT,
+	"SIGALRM":   syscall.SIGALRM,
+	"SIGBUS":    syscall.SIGBUS,
+	"SIGCHLD":   syscall.SIGCHLD,
+	"SIGCLD":    syscall.SIGCLD,
+	"SIGCONT":   syscall.SIGCONT,
+	"SIGFPE":    syscall.SIGFPE,
+	"SIGHUP":    syscall.SIGHUP,
+	"SIGILL":    syscall.SIGILL,
+	"SIGINT":    syscall.SIGINT,
+	"SIGIO":     syscall.SIGIO,
+	"SIGIOT":    syscall.SIGIOT,
+	"SIGKILL":   syscall.SIGKILL,
+	"SIGPIPE":   syscall.SIGPIPE,
+	"SIGPOLL":   syscall.SIGPOLL,
+	"SIGPROF":   syscall.SIGPROF,
+	"SIGPWR":    syscall.SIGPWR,
+	"SIGQUIT":   syscall.SIGQUIT,
+	"SIGSEGV":   syscall.SIGSEGV,
+	"SIGSTKFLT": syscall.SIGSTKFLT,
+	"SIGSTOP":   syscall.SIGSTOP,
+	"SIGSYS":    syscall.SIGSYS,
+	"SIGTERM":   syscall.SIGTERM,
+	"SIGTRAP":   syscall.SIGTRAP,
+	"SIGTSTP":   syscall.SIGTSTP,
+	"SIGTTIN":   syscall.SIGTTIN,
+	"SIGTTOU":   syscall.SIGTTOU,
+	"SIGUNUSED": syscall.SIGUNUSED,
+	"SIGURG":    syscall.SIGURG,
+	"SIGUSR1":   syscall.SIGUSR1,
+	"SIGUSR2":   syscall.SIGUSR2,
+	"SIGVTALRM": syscall.SIGVTALRM,
+	"SIGWINCH":  syscall.SIGWINCH,
+	"SIGXCPU":   syscall.SIGXCPU,
+	"SIGXFSZ":   syscall.SIGXFSZ,
+}
